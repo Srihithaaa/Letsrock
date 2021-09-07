813141bf757a1ae4120f2d058e84dcabe5098621
Srihithaaa <88333325+Srihithaaa@users.noreply.github.com>
2021-09-07 22:49:15 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// Setting time interval
+setInterval(checkBackground,200);
+
+function checkBackground(){
+    generateRandomColor();
+    changeBackgroundColor();
+    changeBackgroundText();
+}
+// Generating random color
+function generateRandomColor(){
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+// Changing background color
+function changeBackgroundColor(){
+   var colorBg = document.getElementById("color-overlay");
+   colorBg.style.backgroundColor = generateRandomColor();
+}
+
+// Changing background text
+function  changeBackgroundText(){
+    var textbg = document.getElementById("text-bg");
+    if(textbg.innerHTML == 'LETS ROCK AND ROLL'){
+    textbg.innerHTML = 'ON THE FLOOR BABY';
+    }
+    else if( textbg.innerHTML == 'ON THE FLOOR BABY'){
+    textbg.innerHTML = 'HIT IT HARD BABY';
+    }
+     else if( textbg.innerHTML == 'HIT IT HARD BABY'){
+    textbg.innerHTML = 'ROCK THE PARTY BABY';
+        }
+    else if( textbg.innerHTML == 'ROCK THE PARTY BABY'){
+    textbg.innerHTML = 'LETS ROCK AND ROLL';
+    }
+}
+
+
+
+
